cfc0375cd743b687313076a1ce57ee3f54ef43ab
adhi1911 <amk2754@gmail.com>
2024-08-08 00:00:06 +0530

    Change feature description at the time of input

--- Student_Performance_Multiple_Regression/app.py
+++ Student_Performance_Multiple_Regression/app.py
@@ -2,40 +2,50 @@ import streamlit as st
 import pickle
 import numpy as np
 
-# Load the models
+# Loading models and scaler
 with open('maths_model.pkl', 'rb') as file:
     maths_model = pickle.load(file)
-
 with open('portuguese_model.pkl', 'rb') as file:
     portuguese_model = pickle.load(file)
-
-# Load the scaler
 with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
-# Define the common features
-common_features = ['higher', 'internet', 'romantic', 'famrel', 'freetime', 'goout', 'Dalc',
-                   'Walc', 'health', 'absences_M']
 
-# Function to make predictions
+common_features = {
+    'higher': 'wants to take higher education (binary: yes or no)',
+    'internet': 'Internet access at home (binary: yes or no)',
+    'romantic': 'with a romantic relationship (binary: yes or no)',
+    'famrel': 'quality of family relationships (numeric: from 1 - very bad to 5 - excellent)',
+    'freetime': 'free time after school (numeric: from 1 - very low to 5 - very high)',
+    'goout': 'going out with friends (numeric: from 1 - very low to 5 - very high)',
+    'Dalc': 'workday alcohol consumption (numeric: from 1 - very low to 5 - very high)',
+    'Walc': 'weekend alcohol consumption (numeric: from 1 - very low to 5 - very high)',
+    'health': 'current health status (numeric: from 1 - very bad to 5 - very good)',
+    'absences_M': 'number of school absences (numeric: from 0 to 93)'
+}
+
+#Predicting the performance
 def predict_performance(model, features):
     features = np.array(features).reshape(1, -1)
     features = scaler.transform(features)  # Apply the scaler
     prediction = model.predict(features)
     return prediction
 
-# Streamlit interface
+# Streamlit app
 st.title('Student Performance Prediction')
 
-# Input fields for features
+#Feature INput
 st.write('Enter the features:')
 features = []
-for feature in common_features:
-    value = st.text_input(f'{feature}', value='0')
-    value = int(value)  # Convert to integer
+for feature, description in common_features.items():
+    if feature in ['higher', 'internet', 'romantic']:
+        value = st.selectbox(f'{feature} ({description})', ['yes', 'no'])
+        value = 1 if value == 'yes' else 0  # Convert to numerical
+    else:
+        value = st.text_input(f'{feature} ({description})', value='0')
+        value = int(value)  # Convert to integer
     features.append(value)
 
-# Make predictions
 if st.button('Predict'):
     maths_prediction = predict_performance(maths_model, features)
     portuguese_prediction = predict_performance(portuguese_model, features)
